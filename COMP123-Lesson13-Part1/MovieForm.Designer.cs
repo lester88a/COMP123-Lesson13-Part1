7ac578633336f61ab3af1a4655f0d0a95b109617
lester88a <lester88a@gmail.com>
2015-04-07 14:19:24 -0400

    part1s

---
+++ COMP123-Lesson13-Part1/MovieForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace COMP123_Lesson13_Part1
+{
+    partial class MovieForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MovieComboBox = new System.Windows.Forms.ComboBox();
+            this.MovieTitleTextBox = new System.Windows.Forms.TextBox();
+            this.MovieTitleLabel = new System.Windows.Forms.Label();
+            this.ListLabel = new System.Windows.Forms.Label();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CategoryTextBox = new System.Windows.Forms.TextBox();
+            this.CostTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MovieComboBox
+            // 
+            this.MovieComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieComboBox.FormattingEnabled = true;
+            this.MovieComboBox.Location = new System.Drawing.Point(21, 35);
+            this.MovieComboBox.Name = "MovieComboBox";
+            this.MovieComboBox.Size = new System.Drawing.Size(186, 28);
+            this.MovieComboBox.Sorted = true;
+            this.MovieComboBox.TabIndex = 0;
+            this.MovieComboBox.SelectedIndexChanged += new System.EventHandler(this.MovieComboBox_SelectedIndexChanged);
+            // 
+            // MovieTitleTextBox
+            // 
+            this.MovieTitleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieTitleTextBox.Location = new System.Drawing.Point(21, 107);
+            this.MovieTitleTextBox.Name = "MovieTitleTextBox";
+            this.MovieTitleTextBox.Size = new System.Drawing.Size(186, 26);
+            this.MovieTitleTextBox.TabIndex = 1;
+            // 
+            // MovieTitleLabel
+            // 
+            this.MovieTitleLabel.AutoSize = true;
+            this.MovieTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieTitleLabel.Location = new System.Drawing.Point(17, 84);
+            this.MovieTitleLabel.Name = "MovieTitleLabel";
+            this.MovieTitleLabel.Size = new System.Drawing.Size(38, 20);
+            this.MovieTitleLabel.TabIndex = 2;
+            this.MovieTitleLabel.Text = "Title";
+            // 
+            // ListLabel
+            // 
+            this.ListLabel.AutoSize = true;
+            this.ListLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListLabel.Location = new System.Drawing.Point(17, 12);
+            this.ListLabel.Name = "ListLabel";
+            this.ListLabel.Size = new System.Drawing.Size(34, 20);
+            this.ListLabel.TabIndex = 3;
+            this.ListLabel.Text = "List";
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox2.Location = new System.Drawing.Point(21, 139);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(186, 26);
+            this.TextBox2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(274, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 204);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CategoryTextBox
+            // 
+            this.CategoryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CategoryTextBox.Location = new System.Drawing.Point(21, 171);
+            this.CategoryTextBox.Name = "CategoryTextBox";
+            this.CategoryTextBox.Size = new System.Drawing.Size(186, 26);
+            this.CategoryTextBox.TabIndex = 6;
+            // 
+            // CostTextBox
+            // 
+            this.CostTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CostTextBox.Location = new System.Drawing.Point(21, 205);
+            this.CostTextBox.Name = "CostTextBox";
+            this.CostTextBox.Size = new System.Drawing.Size(186, 26);
+            this.CostTextBox.TabIndex = 7;
+            // 
+            // MovieForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 254);
+            this.Controls.Add(this.CostTextBox);
+            this.Controls.Add(this.CategoryTextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.ListLabel);
+            this.Controls.Add(this.MovieTitleLabel);
+            this.Controls.Add(this.MovieTitleTextBox);
+            this.Controls.Add(this.MovieComboBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MovieForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Movie Form";
+            this.Load += new System.EventHandler(this.MovieForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox MovieComboBox;
+        private System.Windows.Forms.TextBox MovieTitleTextBox;
+        private System.Windows.Forms.Label MovieTitleLabel;
+        private System.Windows.Forms.Label ListLabel;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox CategoryTextBox;
+        private System.Windows.Forms.TextBox CostTextBox;
+    }
+}
+
---
+++ COMP123-Lesson13-Part1/MovieForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_Lesson13_Part1
+{
+    public partial class MovieForm : Form
+    {
+        //movies array
+        //public MovieClass[] movies = new MovieClass[20];
+
+        public List<MovieClass> movies = new List<MovieClass>();
+
+        public MovieForm()
+        {
+            InitializeComponent();
+            InitializeMovieList();
+
+            InitializeMovieComboBox();
+        }
+
+        private void InitializeMovieList()
+        {
+            //movies[0] = new MovieClass("Season", "Sci-Fi", 2.99);
+            movies.Add(new MovieClass("Season", "Sci-Fi", 2.99));
+            movies.Add(new MovieClass("Dilemma", "Action", 2.99));
+            movies.Add(new MovieClass("Green", "Sci-Fi", 1.99));
+            movies.Add(new MovieClass("Race2", "Sci-Fi", 1.99));
+            
+        }
+
+        private void InitializeMovieComboBox()
+        {
+            foreach (var movie in movies)
+            {
+                //add items of InitializeMovieList() to MovieComboBox
+                MovieComboBox.Items.Add(movie.Title);
+            
+            }
+            
+        }
+        private void MovieComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //showing the inedx
+            //MovieTitleTextBox.Text = MovieComboBox.SelectedIndex.ToString();
+            int movieIndex = MovieComboBox.SelectedIndex;
+
+            //movies.Sort(movies[movieIndex].Title); //sort
+            MovieTitleTextBox.Text = movies[movieIndex].Title;
+            CategoryTextBox.Text = movies[movieIndex].Category;
+            CostTextBox.Text = movies[movieIndex].Cost.ToString();
+            //TextBox2.Text = movies.Select.ToString();
+            //showing the item
+            //TextBox2.Text = MovieComboBox.SelectedItem.ToString();
+            
+        }
+
+        //form load
+        private void MovieForm_Load(object sender, EventArgs e)
+        {
+            //when the form load, the 0 index will be showing on combobox
+            MovieComboBox.SelectedIndex = 0;
+        }
+
+    }
+}
